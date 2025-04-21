d07a8ae98b09f0762c3c51f0dca1d96742ea5735
G4B0CM <Gabriel.calderon.matute@udla.edu.ec>
2025-04-21 17:32:34 -0500

    Palyable animations integrated + particles sistems and collition VFX

--- Assets/Scritps/Weapon.cs
+++ Assets/Scritps/Weapon.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] Animator animator;
+    [SerializeField] GameObject hitVFX;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] int damageAmount = 1;
 
     StarterAssetsInputs starterAssetsInputs;
+    
+
+    const string SHOOT_STRING = "shoot";
 
 
     // Update is called once per frame
     private void Awake()
     {
         starterAssetsInputs = GetComponentInParent<StarterAssetsInputs>();
+        
 
     }
 
@@ -24,13 +31,17 @@ public class Weapon : MonoBehaviour
     private void HandleShoot()
     {
         if (!starterAssetsInputs.shoot) return;
+        muzzleFlash.Play();
+        animator.Play(SHOOT_STRING, 0, 0f);
+        starterAssetsInputs.ShootInput(false);
+
         RaycastHit hit;
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
         {
-
+            
+            Instantiate(hitVFX, hit.point, Quaternion.identity);
             EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
             enemyHealth?.TakeDamage(damageAmount);
-            starterAssetsInputs.ShootInput(false);
         }
     }
 }
